12cba177c0a0463e96b6c3a9f0728088190b3cbc
Jazi <gleb_andreev01@mail.ru>
2025-10-05 22:50:33 +0300

    #7 fix: enable custom security config instead of default generated one

--- src/main/java/eventify/config/CustomSecurityConfig.java
+++ src/main/java/eventify/config/CustomSecurityConfig.java
@@ -6,12 +6,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class CustomSecurityConfig {
 
     private final UserService userService;
